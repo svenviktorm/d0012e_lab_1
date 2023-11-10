af75abfa5d8966b2aea150564324a8abdfaa83d5
Olle Ronstad <ollron-2@student.ltu.se>
2023-11-10 12:05:12 +0100

    changed find index algorithm to binary search

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 fn insertion_sort(array: &mut [isize]){
-    // for each elemnt in the array
+    // for each element in the array
     for i in 1..(array.len()){
         // find insertion index of array[i] in slice 0..i
-        let index = find_insert_index(&array[0..i],array[i]);
+        let index = bi_find_insert_index(&array[0..i],array[i]);
         // insert the value, slice includes index i
         insert_last_at(&mut array[0..=i], index)
     }
 }
 
-fn find_insert_index(slice: &[isize], value: isize) -> usize {
+fn _find_insert_index(slice: &[isize], value: isize) -> usize {
     let mut i = slice.len();
     while i != 0 && value < slice[i-1]{
         i = i-1;
@@ -16,8 +16,23 @@ fn find_insert_index(slice: &[isize], value: isize) -> usize {
     return i;
 }
 
+fn bi_find_insert_index(sub_list: &[isize], value: isize) -> usize {
+    if sub_list.len() == 0 {
+        return 0;
+    }
+    let i: usize = sub_list.len()/2;
+    if value < sub_list[i] {
+        return bi_find_insert_index(&sub_list[0..i], value);
+    }
+    else if value > sub_list[i] {
+        return i+1 + bi_find_insert_index(&sub_list[(i+1)..], value);
+    } else {
+        return i;
+    }
+}
+
 fn insert_last_at(slice: &mut [isize],index: usize){
-    // geting last position index
+    // getting last position index
     let mut j: usize =slice.len()-1;
     // saving our value at the last position
     let value = slice[j];
@@ -31,7 +46,9 @@ fn insert_last_at(slice: &mut [isize],index: usize){
 }
 
 fn main() {
-    let mut my_array = [1,2,6,0];
+    let mut my_array = [1,2,4,6,10,-10];
+    // let index = bi_find_insert_index(&my_array, 3);
+    // println!("found index {:?}",index);
     insertion_sort(&mut my_array);
     println!("{:?}",my_array);
 }
