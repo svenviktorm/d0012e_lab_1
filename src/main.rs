2e12dac99b38166a437d2af658cdbd13ee769f6f
Olle <olle.ronstad@gmail.com>
2023-11-15 15:19:47 +0100

    added dataloging to test growth of function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::{Duration, Instant};
+use std::{time::{Duration, Instant}, fs::File, io::prelude::*};
 
 fn insertion_sort(array: &mut [isize]){
     // for each element in the array
@@ -49,20 +49,20 @@ fn insert_last_at(slice: &mut [isize],index: usize){
 
 fn run_n_sorts_size_m(n:u32,m:usize) -> Duration {
     let mut total_time: Duration = Duration::from_nanos(0);
-    for tests in 0..n{
-        print!("gen data for {:?} out of {:?}; ",tests+1,n);
+    for _tests in 0..n{
+        // print!("gen data for {:?} out of {:?}; ",tests+1,n);
         let mut test_ary: Vec<isize> = Vec::with_capacity(m);
         for _i in 0..m{
             test_ary.push(rand::random());
         };
-        println!("timing; ");
+        // println!("timing; ");
         let now = Instant::now();
         insertion_sort(&mut test_ary);
         total_time = total_time + now.elapsed();
-        println!("done");
-        println!("current average: {:?}",total_time/(tests+1));
+        // println!("done");
+        // println!("current average: {:?}",total_time/(tests+1));
     }
-    return total_time/(n.try_into().unwrap());
+    return total_time/(n);
 }
 
 fn main() {
@@ -71,7 +71,13 @@ fn main() {
     // // println!("found index {:?}",index);
     // insertion_sort(&mut my_array);
     // println!("{:?}",my_array);
-    println!("average time: {:?}", run_n_sorts_size_m(1,1_000_000));
+    let mut data_file = File::create("data.log").unwrap();
+    _ = data_file.write(b"n, time\n");
+    for i in 1..=100{
+        let time: Duration = run_n_sorts_size_m(100, i*1000);
+        let formated_string: String = format!("{}, {:?}\n",i*1000,time);
+        _ = data_file.write(formated_string.as_bytes());
+    }
 }
 
 #[cfg(test)]
