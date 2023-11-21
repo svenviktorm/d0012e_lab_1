0615119782e7646b4f48be15f2d2149febb35390
Olle <olle.ronstad@gmail.com>
2023-11-21 20:48:07 +0100

    made to_graph use matplotlib, add png to ingore

--- to_graph.py
+++ to_graph.py
@@ -1,6 +1,10 @@
 import re
+import sys
+import matplotlib.pyplot as plt 
 
-file = open("run_1_date_20-11/data.wide_k.csv")
+
+path = sys.argv[1]
+file = open(path)
 lines = file.readlines()
 
 def to_mil(in_str):
@@ -12,19 +16,34 @@ def to_mil(in_str):
         number_part = float(re.match("[\d\.]+",in_str)[0])
     elif re.search("s",in_str):
         number_part = float(re.match("[\d\.]+",in_str)[0])*1000
-    return str(number_part)
+    return number_part
     
+n = []
+k_n = []
+k_n_div_2 = []
+k_n_div_4 = []
+k_n_div_16 = []
+k_n_div_32 = []
+k_const_12 = []
 
-for i in range(1,len(lines)):
+lines = lines[2:]
+for i in range(0,len(lines)):
     lines[i] = lines[i].split(", ")
-    lines[i][1] = to_mil(lines[i][1])
-    lines[i][2] = to_mil(lines[i][2])
-    lines[i][3] = to_mil(lines[i][3])
-    lines[i][4] = to_mil(lines[i][4])
-    lines[i][5] = to_mil(lines[i][5])
-    lines[i][6] = to_mil(lines[i][6])
-
-    lines[i] = ("; ".join(lines[i])).replace(".",",")
-lines[0] = lines[0].replace(",",";")
-print("\n".join(lines))
+    n.append(int(lines[i][0]))
+    k_n.append(to_mil(lines[i][1]))
+    k_n_div_2.append(to_mil(lines[i][2]))
+    k_n_div_4.append(to_mil(lines[i][3]))
+    k_n_div_16.append(to_mil(lines[i][4]))
+    k_n_div_32.append(to_mil(lines[i][5]))
+    k_const_12.append(to_mil(lines[i][6]))
+plt.plot(n,k_n,label="k=n")
+plt.plot(n,k_n_div_2,label="k=n/2")
+plt.plot(n,k_n_div_4,label="k=n/4")
+plt.plot(n,k_n_div_16,label="k=n/16")
+plt.plot(n,k_n_div_32,label="k=n/32")
+plt.plot(n,k_const_12,label="k=12")
+plt.legend(["k=n","k=n/2","k=n/4","k=n/16","k=n/32","k=12"])
+plt.xlabel("n")
+plt.ylabel("Time ms")
+plt.savefig(path+".png")
 
